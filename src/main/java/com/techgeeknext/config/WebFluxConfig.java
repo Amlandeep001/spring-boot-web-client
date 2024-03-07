14cf0649ead56dff10a8fb30789032fe6b5f4f6d
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2024-03-08 00:59:30 +0530

    Logging changes for Webclient
    Logging changes for Webclient

--- src/main/java/com/techgeeknext/config/WebFluxConfig.java
+++ src/main/java/com/techgeeknext/config/WebFluxConfig.java
@@ -19,21 +19,22 @@ import reactor.netty.http.client.HttpClient;
 @EnableWebFlux
 public class WebFluxConfig implements WebFluxConfigurer
 {
-    @Bean
-    WebClient getWebClient()
-    {
-        HttpClient httpClient = HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
-                .doOnConnected(conn -> conn
-                        .addHandlerLast(new ReadTimeoutHandler(10))
-                        .addHandlerLast(new WriteTimeoutHandler(10)));
+	@Bean
+	WebClient getWebClient()
+	{
+		HttpClient httpClient = HttpClient.create()
+				.wiretap(true) // for logging request and response 
+				.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+				.doOnConnected(conn -> conn
+						.addHandlerLast(new ReadTimeoutHandler(10))
+						.addHandlerLast(new WriteTimeoutHandler(10)));
 
-        ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient.wiretap(true));
+		ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient.wiretap(true));
 
-        return WebClient.builder()
-                .baseUrl("http://localhost:8060")
-                .clientConnector(connector)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-    }
+		return WebClient.builder()
+				.baseUrl("http://localhost:8060")
+				.clientConnector(connector)
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				.build();
+	}
 }
