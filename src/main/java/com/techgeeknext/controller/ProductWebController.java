8bd4a48f8fc2f938327f3ca03e67d960a0a7be41
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2024-05-06 01:29:33 +0530

    Reformatting

--- src/main/java/com/techgeeknext/controller/ProductWebController.java
+++ src/main/java/com/techgeeknext/controller/ProductWebController.java
@@ -1,6 +1,5 @@
 package com.techgeeknext.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,49 +15,56 @@ import org.springframework.web.bind.annotation.RestController;
 import com.techgeeknext.model.Product;
 import com.techgeeknext.service.ProductService;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/products")
-public class ProductWebController 
+public class ProductWebController
 {
-	@Autowired
-	private ProductService productService;
+	private final ProductService productService;
 
 	@GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	@ResponseStatus(HttpStatus.OK)
-	//using Flux for collection of employees
-	public Flux<Product> findAll() {
+	// using Flux for collection of employees
+	public Flux<Product> findAll()
+	{
 		return productService.findAll();
 	}
 
 	@GetMapping(value = "/{id}")
-	//using Mono for single employee
-	public Mono<Product> findById(@PathVariable("id") Long id) {
+	// using Mono for single employee
+	public Mono<Product> findById(@PathVariable("id") Long id)
+	{
 		return productService.findById(id);
 	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	//using Mono for single employee
-	public Mono<Product> create(@RequestBody Product p) {
+	// using Mono for single employee
+	public Mono<Product> create(@RequestBody Product p)
+	{
 		return productService.create(p);
 	}
 
 	@PutMapping(value = "/{id}")
 	@ResponseStatus(HttpStatus.OK)
-	//using Mono for single employee
-	public Mono<Product> update(@RequestBody Product p, @PathVariable("id") Long id) {
+	// using Mono for single employee
+	public Mono<Product> update(@RequestBody Product p, @PathVariable("id") Long id)
+	{
 		p.setId(id);
 		Mono<Product> prd = productService.update(p);
-		if(prd != null) {
+		if(prd != null)
+		{
 			log.info("Response :: {}", prd.toString());
 			return prd;
 		}
-		else {
+		else
+		{
 			throw new RuntimeException("Got null response back as ID does not exist in DB");
 		}
 	}
@@ -66,7 +72,8 @@ public class ProductWebController
 	@DeleteMapping(value = "/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	// using Mono<Void> when no response content is expected
-	public Mono<Boolean> delete(@PathVariable("id") Long id) {
+	public Mono<Boolean> delete(@PathVariable("id") Long id)
+	{
 		return productService.delete(id);
 	}
 }
